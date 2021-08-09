3c36b00e7f263ad66cf5488d3a969522111e8c6c
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-08-09 16:50:11 +0200

    Team service

--- src/main/java/finalexam/TeamService.java
+++ src/main/java/finalexam/TeamService.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class TeamService {
 
     private TeamRepository teamRepository;
 
-    private PlayerService playerService;
+    private PlayerRepository playerRepository;
 
     private ModelMapper modelMapper;
 
@@ -25,12 +26,29 @@ public class TeamService {
     }
 
     public TeamDTO createTeam(CreateTeamCommand command) {
-        return teamRepository.save(command);
+        Team team = teamRepository.save(new Team(command.getName()));
+        return modelMapper.map(team, TeamDTO.class);
     }
 
-    public void addPlayerToTheTeam(UpdateWithExistingPlayerCommand command) {
+    @Transactional
+    public TeamDTO addPlayerToTheTeam(long id, CreatePlayerCommand command) {
+        Team team = findPlayerById(id);
+        Player player = playerRepository.save(new Player(command.getName(), command.getBirthDate(), command.getPosition()));
+        team.addNewPlayer(player);
+        return modelMapper.map(team, TeamDTO.class);
     }
 
-    public void buyPlayer() {
+    @Transactional
+    public TeamDTO buyPlayer(long id, UpdateWithExistingPlayerCommand command) {
+        Player player = playerRepository.findById(command.getId()).orElseThrow(()-> new PlayerOrTeamNotFoundException("Player"));
+        Team team = findPlayerById(id);
+        if (player.getTeam() == null) {
+            team.addNewPlayer(player);
+        }
+        return modelMapper.map(team, TeamDTO.class);
+    }
+
+    private Team findPlayerById(Long id) {
+        return teamRepository.findById(id).orElseThrow(() -> new PlayerOrTeamNotFoundException("Player"));
     }
 }
