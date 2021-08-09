7a24a85a0bcd48539af1fe302478aabebed21dca
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-08-09 16:50:40 +0200

    Own Exception

---
+++ src/main/java/finalexam/PlayerOrTeamNotFoundException.java
@@ -0,0 +1,18 @@
+package finalexam;
+
+import org.zalando.problem.AbstractThrowableProblem;
+import org.zalando.problem.Status;
+
+import java.net.URI;
+
+public class PlayerOrTeamNotFoundException extends AbstractThrowableProblem {
+
+    public PlayerOrTeamNotFoundException(String entity) {
+        super(URI.create("entity-not-found"),
+                entity + " not found",
+                Status.NOT_FOUND,
+                String.format(entity + " not found")
+        );
+    }
+
+}
